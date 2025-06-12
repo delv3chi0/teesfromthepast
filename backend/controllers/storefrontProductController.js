fbc889debdd408c1fdfba7fb4982cc7885120d57
Damon Delvechio <delv3chio@gmail.com>
2025-06-11 18:34:50 -0700

    Product Type Update

--- backend/controllers/storefrontProductController.js
+++ backend/controllers/storefrontProductController.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-    // MODIFIED: Importing the new, correct functions from the controller
     getActiveProductsByCategory,
     getShopData,
     getProductBySlug,
@@ -8,13 +7,10 @@ import {
 
 const router = express.Router();
 
-// This route gets all the data needed for the main shop page
 router.get('/shop', getShopData);
 
-// MODIFIED: This route now correctly gets products by a category ID
 router.get('/products/category/:categoryId', getActiveProductsByCategory);
 
-// This route gets a single product by its unique slug for the detail page
 router.get('/products/:slug', getProductBySlug);
 
 export default router;
