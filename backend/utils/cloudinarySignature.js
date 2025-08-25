107303027da958aa1834d8a8697edefa79f8a6f4
delv3chi0 <delv3chio@gmail.com>
2025-08-25 07:18:05 -0700

    Update cloudinarySignature.js

--- backend/utils/cloudinarySignature.js
+++ backend/utils/cloudinarySignature.js
@@ -1,19 +1,25 @@
 import crypto from "crypto";
 
 /**
- * Generate a Cloudinary SHA-1 signature given params (excluding file & api_key & signature).
- * params should be an object of upload parameters (folder, public_id, eager, timestamp, etc.)
+ * Generate a Cloudinary SHA-1 signature.
+ * Only include upload parameters you actually send to Cloudinary on the client.
+ * Exclude: file, resource_type auto detection, api_key, signature (Cloudinary adds those).
+ *
+ * @param {object} params - key/value pairs for upload (folder, public_id, timestamp, etc.)
+ * @param {string} apiSecret - CLOUDINARY_API_SECRET
+ * @returns {{ signature: string, toSign: string }}
  */
 export function generateCloudinarySignature(params, apiSecret) {
-  // Cloudinary requires params sorted alphabetically, joined as key=value&... then apiSecret appended.
-  const sortedKeys = Object.keys(params).sort();
-  const toSign = sortedKeys
+  const sortedKeys = Object.keys(params)
     .filter((k) => params[k] !== undefined && params[k] !== null && params[k] !== "")
-    .map((k) => `${k}=${params[k]}`)
-    .join("&");
+    .sort();
+
+  const toSign = sortedKeys.map((k) => `${k}=${params[k]}`).join("&");
   const signature = crypto
     .createHash("sha1")
     .update(toSign + apiSecret)
     .digest("hex");
   return { signature, toSign };
 }
+
+export default generateCloudinarySignature;
