38cdfb11b0ba54c8a80eb1c49ca98991577329bf
delv3chi0 <delv3chio@gmail.com>
2025-08-25 06:16:09 -0700

    Refactor request ID middleware exports and comments

--- backend/middleware/requestId.js
+++ backend/middleware/requestId.js
@@ -1,17 +1,21 @@
 /**
- * Request ID middleware (ESM version using nanoid).
- * Generates a short, collision-resistant ID per request.
+ * Request ID middleware (ESM) using nanoid.
+ * Exports:
+ *   - named: requestId
+ *   - default: requestId
  *
- * Usage (in app.js before other middlewares that log):
- *   import requestId from './middleware/requestId.js';
- *   app.use(requestId);
+ * This allows either:
+ *   import { requestId } from "./middleware/requestId.js";
+ *   import requestId from "./middleware/requestId.js";
  */
-import { nanoid } from 'nanoid';
-import { REQUEST_ID_HEADER } from '../config/constants.js';
+import { nanoid } from "nanoid";
+import { REQUEST_ID_HEADER } from "../config/constants.js";
 
-export default function requestId(req, res, next) {
-  const id = nanoid(10); // 10-char ID keeps logs compact
+export function requestId(req, res, next) {
+  const id = nanoid(10); // Compact, collision-resistant enough for request tracing
   req.id = id;
   res.setHeader(REQUEST_ID_HEADER, id);
   next();
 }
+
+export default requestId;
