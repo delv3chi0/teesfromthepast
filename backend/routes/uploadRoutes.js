4a9345b0d3555aeeb55d2c6203e353bf07da92e9
delv3chi0 <delv3chio@gmail.com>
2025-08-25 06:30:57 -0700

    Remove comments from uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,9 +1,3 @@
-/**
- * Route wiring for print file upload.
- * Ensure this is mounted in app.js, e.g.:
- *   import uploadRoutes from "./routes/uploadRoutes.js";
- *   app.use("/api/upload", uploadRoutes);
- */
 import { Router } from "express";
 import { uploadPrintFile } from "../controllers/uploadController.js";
 
